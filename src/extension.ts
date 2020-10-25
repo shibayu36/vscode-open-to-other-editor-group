9d91c6243fcad80e06bf911bad585b81df4c051d
shibayu36 <shibayu36@gmail.com>
2020-10-26 00:21:03 +0900

    Make command to open focused file in next editor group

--- src/extension.ts
+++ src/extension.ts
@@ -12,11 +12,9 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("open-to-other-editor-group.helloWorld", () => {
+  let disposable = vscode.commands.registerCommand("open-to-other-editor-group.openToOtherEditorGroup", () => {
     // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from open-to-other-editor-group!");
+    openToOtherEditorGroup();
   });
 
   context.subscriptions.push(disposable);
@@ -24,3 +22,35 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+async function openToOtherEditorGroup(): Promise<void> {
+  await vscode.commands.executeCommand("copyFilePath");
+  const filename = await vscode.env.clipboard.readText();
+
+  let uri: vscode.Uri;
+  try {
+    uri = vscode.Uri.parse(filename);
+  } catch {
+    return;
+  }
+
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return;
+  }
+
+  const editors = vscode.window.visibleTextEditors;
+  let columnToOpen: number;
+  if (editors.length <= 1) {
+    // If there is only one editor group, create a second one.
+    columnToOpen = -2;
+  } else if (activeEditor.viewColumn === editors.length) {
+    // If you are in the last editorGroup, show in the first editorGroup
+    columnToOpen = 1;
+  } else {
+    // Show in the next editorGroup
+    columnToOpen = activeEditor.viewColumn ? activeEditor.viewColumn + 1 : 1;
+  }
+
+  await vscode.commands.executeCommand("vscode.open", uri, columnToOpen);
+}
